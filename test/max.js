eeae0059d094e099a85307d0de8d4cf30fcc3a33
yeahisme <zhangdongsheng_888@126.com>
2017-04-07 02:16:26 +0800

    Create max.js

---
+++ test/max.js
@@ -0,0 +1,8 @@
+var maxBy = require('../index');
+var test = require('tape');
+
+test('simple comparisons', function (t) {
+    t.plan(1);
+    var n = maxBy([9,3,4], function (x) { return x % 3 });
+    t.equal(n, 4);
+});
